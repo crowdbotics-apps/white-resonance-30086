a7d07109d8d1ca3adce0f09f1706a81d0f59296d
Crowdbotics <team@crowdbotics.com>
2021-08-24 22:39:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/white_resonance_30086/MainActivity.java
@@ -0,0 +1,15 @@
+package com.white_resonance_30086;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "white_resonance_30086";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "white_resonance_30086Identifier",
+  url: "https://white_resonance_30086Identifier.botics.co",
+  api: "https://white_resonance_30086Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
